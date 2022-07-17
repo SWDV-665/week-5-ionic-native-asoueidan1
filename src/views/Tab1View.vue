b21dd7f5a38d3302195350697b8402df9aee18a1
nayzakcreations <nayzakcreations@gmail.com>
2022-07-17 12:20:20 -0500

    1

--- src/views/Tab1View.vue
+++ src/views/Tab1View.vue
@@ -1,30 +1,82 @@
 <template>
   <ion-page>
-    <ion-list>
-      <ion-item v-for="grocery in groceries" :key="grocery.name">
-        <ion-label>{{ grocery.name }} ({{ grocery.quantity }})</ion-label>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>List</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content fullscreen>
+      <!--      ADD ITEM NAME INPUT-->
+      <ion-item>
+        <ion-label>Item name</ion-label>
+        <ion-input v-model="itemName"></ion-input>
       </ion-item>
-    </ion-list>
+      <!--      ADD ITEM QUANTITY INPUT-->
+      <ion-item>
+        <ion-label>Item quantity</ion-label>
+        <ion-input type="number" v-model="itemQuantity"></ion-input>
+      </ion-item>
+      <!--      ADD ITEM BUTTON-->
+      <ion-button :disabled="addItemBtnDisabled" expand="block" @click="addItem">Add item</ion-button>
+      <!--      THE LIST-->
+      <ion-list>
+        <!--        Enable item sliding-->
+        <ion-item-sliding v-for="(grocery, index) in groceries" :key="grocery.name">
+          <ion-item>
+            <ion-label>{{ grocery.name }} ({{ grocery.quantity }})</ion-label>
+          </ion-item>
+          <!--          Incorporate a delete button in each item-->
+          <ion-item-options>
+            <ion-button @click="deleteItem(index)" color="danger">
+              Delete
+            </ion-button>
+          </ion-item-options>
+        </ion-item-sliding>
+      </ion-list>
+    </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
 import {
-  IonPage, IonList, IonItem, IonLabel,
+  IonPage, IonList, IonItem, IonLabel, IonInput, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItemSliding, IonItemOptions
 } from '@ionic/vue';
 import {getGroceries} from "@/data/groceries";
 
 export default {
   name: "Tab1View",
   components: {
+    IonInput,
     IonPage,
     IonList,
     IonItem,
     IonLabel,
+    IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItemSliding, IonItemOptions
   },
   data: () => ({
     groceries: getGroceries(),
+    itemName: '',
+    itemQuantity: '',
   }),
+  methods: {
+    // ADD ITEM FUNCTION
+    addItem() {
+      const groceries = this.groceries;
+      groceries.push({name: this.itemName.trim(), quantity: parseInt(this.itemQuantity)})
+      this.itemName = ''
+      this.itemQuantity = ''
+    },
+    // DELETE ITEM FUNCTION
+    deleteItem(itemIndex) {
+      this.groceries.splice(itemIndex, 1)
+    },
+  },
+  computed: {
+    addItemBtnDisabled(): boolean {
+      return !this.itemName || !this.itemQuantity
+    }
+  },
+
 }
 </script>
 
